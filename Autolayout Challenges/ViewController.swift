2e3ecb2d1aaf05ab15005cb0228f451d4bc3f1ab
jhaff <jacobhaff2@gmail.com>
2018-10-25 15:05:30 -0700

    Initial Commit

---
+++ Autolayout Challenges/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Autolayout Challenges
+//
+//  Created by Jacob Haff on 10/25/18.
+//  Copyright © 2018 Jacob Haff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
