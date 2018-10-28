91cb5e81531eb3e8667f0ee850c87612d84b74c0
jhaff <jacobhaff2@gmail.com>
2018-10-28 10:30:59 -0700

    Multiple view controllers. Completed second challenge (1/3 and 2/3 boxes)

--- Autolayout Challenges/ViewController.swift
+++ Autolayout Challenges/ViewController.swift
@@ -8,32 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController2: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-//        let tealView = UIView()
-//        tealView.backgroundColor = UIColor.green
-//        tealView.translatesAutoresizingMaskIntoConstraints = false
-//        
-//        let tealTop = NSLayoutConstraint(item: tealView, attribute: .top, relatedBy: .equal, toItem: self.view, attribute: .top, multiplier: 1.0, constant: 0)
-//        
-//        let tealHeight = NSLayoutConstraint(item: tealView, attribute: .height, relatedBy: .equal, toItem: self.view, attribute: .height, multiplier: 0.5, constant: 0)
-//        
-//        let tealLeading = NSLayoutConstraint(item: tealView, attribute: .leading, relatedBy: .equal, toItem: self.view, attribute: .leading, multiplier: 1.0, constant: 0)
-//        
-//        let tealWidth = NSLayoutConstraint(item: tealView, attribute: .width, relatedBy: .equal, toItem: self.view, attribute: .width, multiplier: 1.0, constant: 0)
-//        
-//        self.view.addSubview(tealView) //before constraints activated req
-//        
-//        NSLayoutConstraint.activate([tealTop,tealWidth,tealHeight,tealLeading])
-////
-//        NSLayoutConstraint.activate([tealView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
-//                                     tealView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),])
-//        //
+        let tealView = UIView()
+        tealView.backgroundColor = UIColor.green
+        tealView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let tealTop = NSLayoutConstraint(item: tealView, attribute: .top, relatedBy: .equal, toItem: self.view, attribute: .top, multiplier: 1.0, constant: 0)
+        
+        let tealHeight = NSLayoutConstraint(item: tealView, attribute: .height, relatedBy: .equal, toItem: self.view, attribute: .height, multiplier: 0.5, constant: 0)
+        
+        let tealLeading = NSLayoutConstraint(item: tealView, attribute: .leading, relatedBy: .equal, toItem: self.view, attribute: .leading, multiplier: 1.0, constant: 0)
+        
+        let tealWidth = NSLayoutConstraint(item: tealView, attribute: .width, relatedBy: .equal, toItem: self.view, attribute: .width, multiplier: 1.0, constant: 0)
+        
+        self.view.addSubview(tealView) //before constraints activated req
+        
+        NSLayoutConstraint.activate([tealTop,tealWidth,tealHeight,tealLeading])
 //
+        NSLayoutConstraint.activate([tealView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+                                     tealView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),])
+        //
+
 
         let blueView = UIView()
         blueView.backgroundColor = UIColor.blue
@@ -46,8 +46,46 @@ class ViewController: UIViewController {
                                      blueView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.5),
                                      blueView.widthAnchor.constraint(equalTo: self.view.widthAnchor),
                                      ])
-        
     }
-
 }
 
+class ViewController: UIViewController { //for the second challenges
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let tealView = UIView()
+        tealView.backgroundColor = UIColor.green
+        tealView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let tealTop = NSLayoutConstraint(item: tealView, attribute: .top, relatedBy: .equal, toItem: self.view, attribute: .top, multiplier: 1.0, constant: 0)
+        
+        let tealHeight = NSLayoutConstraint(item: tealView, attribute: .height, relatedBy: .equal, toItem: self.view, attribute: .height, multiplier: 1/3, constant: 0)
+        
+        let tealLeading = NSLayoutConstraint(item: tealView, attribute: .leading, relatedBy: .equal, toItem: self.view, attribute: .leading, multiplier: 1.0, constant: 0)
+        
+        let tealWidth = NSLayoutConstraint(item: tealView, attribute: .width, relatedBy: .equal, toItem: self.view, attribute: .width, multiplier: 1.0, constant: 0)
+        
+        self.view.addSubview(tealView) //before constraints activated req
+        
+        NSLayoutConstraint.activate([tealTop,tealWidth,tealHeight,tealLeading])
+        //
+        NSLayoutConstraint.activate([tealView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+                                     tealView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),])
+        //
+        
+        
+        let blueView = UIView()
+        blueView.backgroundColor = UIColor.blue
+        blueView.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.view.addSubview(blueView)
+        
+        NSLayoutConstraint.activate([blueView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+                                     blueView.topAnchor.constraint(equalTo: tealView.bottomAnchor),
+                                     blueView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 2/3),
+                                     blueView.widthAnchor.constraint(equalTo: self.view.widthAnchor),
+                                     ])
+    }
+}
